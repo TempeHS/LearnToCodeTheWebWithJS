b8e920f91fb4ca2790822c2e1af5cfe2fbe6e366
Ben Jones <benpaddlejones@outlook.com>
2023-03-18 20:28:56 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+alert("Hello, world"); //Output to windows alert box
+document.getElementById("ScriptThisDIV").innerHTML = "Hello, world"; //Output to the DOM (Document Object Module) with the id="ScriptThisDIV"
+console.log("Hello, world"); //Output to the console (Open dev tools and look at the console)
